4dbcbf93303dd500fc8020162651f724e03b1e96
catmiller07 <catmiller07@comcast.net>
2015-10-02 20:58:49 -0400

    Switch-case

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var g_note=document.getElementById("g_note");
 var a_note=document.getElementById("a_note"); 
 var b_note=document.getElementById("b_note"); 
 
-    $("#c").on("click mouseover",function(){
+    /*$("#c").on("click mouseover",function(){
         c_note.currentTime=0;
         c_note.play();
     });
@@ -35,7 +35,43 @@ var b_note=document.getElementById("b_note");
         c_note.currentTime=0;
         c_note.play();
     });
+    */
+    //attach listener for keypresses
     
+    $(document).on("keypress",function(evt){
+        console.log(evt.keyCode);
+        switch(evt.keyCode) {
+            case 97:
+                    c_note.currentTime=0;
+                    c_note.play();
+                    break;
+            case 115:
+                    d_note.currentTime=0;
+                    d_note.play();
+                    break;
+            case 100:
+                    e_note.currentTime=0;
+                    e_note.play();
+                    break;
+            case 102:
+                    f_note.currentTime=0;
+                    f_note.play();
+                    break;
+            case 103:
+                    g_note.currentTime=0;
+                    g_note.play();
+                    break;
+            case 104:
+                    a_note.currentTime=0;
+                    a_note.play();
+                    break;
+             case 106:
+                    b_note.currentTime=0;
+                    b_note.play();
+                    break;
+        }
+        
+    });
 });                  
                
 
